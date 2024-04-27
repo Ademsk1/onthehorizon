7017e3da9eb7c8716a9c68ec67bf6c37d3174087
Ademsk1 <adam.illoul24@gmail.com>
2024-04-27 13:33:18 +0100

    feat: 3d

---
+++ calculate.js
@@ -0,0 +1,4 @@
+//lat: angle between equatorial plane and the straight line that passes through that point and through the center of the earth
+//long: angle east or west of a reference meridian to another meridian that passes through that point
+
+const getCoordinates = (latitude, longitude, direction) => {};
--- pythontests/test3d.py
+++ pythontests/test3d.py
@@ -3,5 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from test import new_position
 
+EARTH_R = 6.371e4
 
 
+def get_coordinate_projection(lat, long, direction, distance, spacing):
+    # lat, long and dir should be 0-360. 
+    latR, longR, directionR = np.deg2rad([lat, long, direction])
+    steps = np.arange(0, distance, spacing)
+    latitudes = steps * np.cos(directionR) / EARTH_R + latR
+    longitudes = steps * np.sin(directionR) / EARTH_R + longR
+    return latitudes, longitudes
+
+
+
+def main():
+    start = np.array([51.5072,0.1276])
+    lats, longs = get_coordinate_projection(start[0], start[1], 0, 3e6, 50)
+    x = np.cos(lats) * np.cos(longs)
+    y = np.cos(lats) *np.sin(longs)
+    z = np.sin(lats)
+    ax = plt.axes(projection='3d')
+    ax.scatter(x[0], y[0], z[0])
+    ax.plot3D(x,y,z)
+    
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
